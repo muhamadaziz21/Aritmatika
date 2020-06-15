0684ef0ace00eff2be2f9c8c0bbbb031a179cf85
RifkiMaulana123 <maulanfikri98@gmail.com>
2020-06-15 13:35:14 +0700

    fungsi mod

--- menu_program.c
+++ menu_program.c
@@ -6,31 +6,63 @@ Nama Program: menu_program.c
 #include <stdio.h>
 
 double tambah(double a, double b) {
-	return a + b;
+ return a + b;
 }
 
+double kurang(double a, double b) {
+ return a - b;
+}
+double kali(double a, double b) {
+ return a - b;
+}
+double bagi (double a, double b){
+    return a / b;
+    }
+double mod (double a, double b){
+    return a % b;
+    }
+
 int main() {
-	int pilihan;
-	double angka1, angka2;
+ int pilihan;
+ double angka1, angka2;
 
-	printf("Menu Program\n");
-	printf("----------------------\n");
-	printf("1. Tambah\n");
+ printf("Menu Program\n");
+ printf("----------------------\n");
+ printf("1. pertambahan\n");
+ printf("2. pengurangan\n");
+ printf("3. perkalian\n");
+ printf("4. pembagian\n");
+ printf("5. modular\n");
 
-	printf("Masukkan pilihan Anda : ");
-	scanf("%d", &pilihan);
+ printf("Masukkan pilihan Anda : ");
+ scanf("%d", &pilihan);
 
-	printf("Masukkan Angka I : ");
-	scanf("%lf",&angka1);
-	printf("Masukkan Angka II : ");
-	scanf("%lf",&angka2);
+ printf("Masukkan Angka I : ");
+ scanf("%lf",&angka1);
+ printf("Masukkan Angka II : ");
+ scanf("%lf",&angka2);
 
-	switch (pilihan) 
-	{
-		case 1:
-			printf("Hasil penjumlahan : ");
-			printf("%.1f\n", tambah(angka1, angka2));
-		break;
-	}
-	return 0;
+ switch (pilihan)
+ {
+  case 1:
+   printf("Hasil penjumlahan : ");
+   printf("%.1f\n", tambah(angka1, angka2));
+  break;
+  case 2:
+   printf("Hasil pengurangan : ");
+   printf("%.1f\n", kurang(angka1, angka2));
+  break;
+  case 3:
+   printf("Hasil perkalian : ");
+   printf("%.1f\n", kali(angka1, angka2));
+  break;
+  case 4:
+            printf("Hasil pembagian : ");
+            printf("%.1f\n", bagi(angka1, angka2));
+        break;
+        case 5:
+            printf("Hasil modular : ");
+            printf("%.1f\n", mod(angka1, angka2));
+ }
+ return 0;
 }
